c8c70689c73a3a664b8bf24dc33c3b21add8121a
vincent phuah <vphuah@gmail.com>
2016-01-20 03:12:05 +0800

    back

---
+++ PuresRakuten/model/productModel.php
@@ -0,0 +1,27 @@
+<?php
+
+final class productModel {
+
+    var $name = "";
+    var $sku = "";
+    var $primary = "";
+    var $secondary = "";
+    //URL
+    var $productUrl = "";
+    var $productImage = "";
+    var $buyLink = "";
+    //description
+    var $short_description = "";
+    var $long_description = "";
+    //price
+    var $price_currency = "";
+    var $retail = 0;
+    var $final_price = 0;
+    //discount
+    var $discount_currency = "";
+    var $discount_amount = 0;
+    var $brand = "";
+
+}
+
+?>
